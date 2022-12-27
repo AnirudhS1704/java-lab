c82ab94ce1274ebfd6e35fa1991e496f27a2c443
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2022-12-27 12:33:48 +0530

    Add files via upload

---
+++ ISE_department.java
@@ -0,0 +1,26 @@
+package Q10.ISE;
+import Q10.Department;
+import Q10.Faculty;
+
+public class ISE_department implements Department{
+
+    @Override
+    public void readData() {
+        System.out.println(" ");
+    }
+
+    @Override
+    public void printData() {
+
+    }
+
+    @Override
+    public void print_no_desinations() {
+
+    }
+
+    @Override
+    public void no_research_consltncy_projs() {
+
+    }
+}
