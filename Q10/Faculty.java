28d261282e3466f40b41557129f6156d722b6d35
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2022-12-28 23:17:07 +0530

    Create Faculty.java

---
+++ Q10/Faculty.java
@@ -0,0 +1,12 @@
+package Q10;
+
+public class Faculty {
+    public String name;
+    public String designation;
+    public String joining_date;
+    public String subjects_handled;
+    public int age;
+    public int yoe;
+    public int no_research_consltncy_projs;
+    public int no_designations;
+}
