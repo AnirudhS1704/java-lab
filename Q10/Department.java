215f1ac6f13c9bc0b1dba5514dc84984d9850c5e
Anirudh S <107941714+AnirudhS1704@users.noreply.github.com>
2022-12-28 23:16:24 +0530

    Create Department.java

---
+++ Q10/Department.java
@@ -0,0 +1,10 @@
+package Q10;
+import Q10.Faculty;
+
+public interface Department {
+    void readData(Faculty f);
+    void printData(Faculty f);
+    void print_no_desinations(Faculty f);
+    void no_research_consltncy_projs(Faculty f);
+}
+
